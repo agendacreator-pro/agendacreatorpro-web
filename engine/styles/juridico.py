3bf89eeab8489eb0bdfbf1aa65d61bfe8b9e6d0a
Agenda Creator Pro <agendacreatorpro@gmail.com>
2026-08-07 18:10:38 -0300

    Agenda juridica com agendamentos: caixa Prazos e Anotacoes sobe para junto do ultimo horario (18h) e ganha mais linhas

--- engine/styles/juridico.py
+++ engine/styles/juridico.py
@@ -478,8 +478,8 @@ class Juridico(EstiloBase):
                 pdf.setLineWidth(0.2)
                 pdf.line(sx(24 * mm), sy((slot_y - 2) * mm), sx((pw - 12) * mm), sy((slot_y - 2) * mm))
 
-            self._moldura_caixa(pdf, 8, 14, gw, 28, localization.label("prazos_anotacoes"))
-            self._linhas(pdf, 8, 14, gw, 28, intervalo=5)
+            self._moldura_caixa(pdf, 8, 14, gw, ph - 150, localization.label("prazos_anotacoes"))
+            self._linhas(pdf, 8, 14, gw, ph - 150, intervalo=6)
         else:
             self._moldura_caixa(pdf, 8, ph - 46, pw - 16, 12, localization.label("prazos_dia"))
             self._linhas(pdf, 8, ph - 46, pw - 16, 12, intervalo=4)
